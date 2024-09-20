0b277a97182a6d4e25e426c0ffb3c798b31a3a8d
bhumitgoyal <lifestyleprojects.rg@gmail.com>
2024-09-20 14:56:13 +0530

    Basic Postgre connection

---
+++ src/test/kotlin/com/example/vitclubactivityhub/VitclubactivityhubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vitclubactivityhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VitclubactivityhubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
